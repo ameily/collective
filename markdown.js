3e9f18b104f8bb9aaf927211ab00acd99236b5e4
Adam Meily <meily.adam@gmail.com>
2015-01-22 17:13:30 -0500

    initial commit of markdown rendering

---
+++ markdown.js
@@ -0,0 +1,21 @@
+
+var md = require('markdown-it')({
+    breaks: true,
+    linkify: true
+}).use(require('markdown-it-hashtag'), {
+    hashtagRegExp: "\\d+"
+});
+
+md.renderer.rules.hashtag_open = function(tokens, idx) {
+    return '<a href="#" class="issue" data-issue="' + tokens[idx].content + '">';
+};
+
+md.renderer.rules.hashtag_text = function(tokens, idx) {
+    return '#' + tokens[idx].content;
+};
+
+md.renderer.rules.hashtag_close = function() {
+    return "</a>";
+};
+
+module.exports = md;
